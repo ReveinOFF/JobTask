820247725bf6164752925f99338a405e2e8d7346
RonniePlay <ronnieplayyt@gmail.com>
2023-07-14 15:26:46 +0300

    Completed tasks
    All major tasks completed

---
+++ src/modules/beers/store.js
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+export const useStoreBeers = create(
+  devtools((set) => ({
+    data: [],
+    isLoading: true,
+    addData: (beers) =>
+      set(() => ({
+        isLoading: true,
+        data: [...beers],
+        isLoading: false,
+      })),
+    removeData: (beerId) =>
+      set((state) => ({
+        isLoading: true,
+        data: state.data.filter((beer) => beer.id !== beerId),
+        isLoading: false,
+      })),
+  }))
+);
